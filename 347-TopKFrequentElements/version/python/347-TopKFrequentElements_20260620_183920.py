36ec7a5f1065e26cdc133a5609c37da51f6495c9
harsha0103 <nirmalharsha0103@gmail.com>
2026-06-20 18:39:21 -0700

    Create 347-TopKFrequentElements

---
+++ 347-TopKFrequentElements/version/python/347-TopKFrequentElements_20260620_183920.py
@@ -0,0 +1,41 @@
+# Last updated: 6/20/2026, 6:39:20 PM
+# encode & Decode
+1class Codec:
+2
+3    def encode(self, strs):
+4        """Encodes a list of strings to a single string.
+5        
+6        :type strs: List[str]
+7        :rtype: str
+8        """
+9        res=[]
+10        for s in strs:
+11            res.append(str(len(s)))
+12            res.append("#")
+13            res.append(s)
+14        return ''.join(res)
+15
+16    def decode(self, s):
+17        """Decodes a single string to a list of strings.
+18        
+19        :type s: str
+20        :rtype: List[str]
+21        """
+22        arr=[]
+23        i=0
+24        while i < len(s):
+25            j=i
+26            while s[j]!='#':
+27                j+=1
+28            num=int(s[i:j])
+29            j+=1
+30
+31            new_str=s[j:j+num]
+32            arr.append(new_str)
+33            i=j+num
+34        return arr
+35        
+36
+37# Your Codec object will be instantiated and called as such:
+38# codec = Codec()
+39# codec.decode(codec.encode(strs))

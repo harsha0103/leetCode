2c55d8414cb688b627760407731e40c5d25ec691
harsha0103 <nirmalharsha0103@gmail.com>
2026-06-25 09:16:15 -0700

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.py
@@ -0,0 +1,13 @@
+# Last updated: 6/25/2026, 9:16:15 AM
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res =0
+
+        for i in nums:
+            res^=i
+        
+        return res

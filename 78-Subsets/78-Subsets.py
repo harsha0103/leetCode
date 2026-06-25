54f800631550c8a2ba4f10c20bbc5ba203bee620
harsha0103 <nirmalharsha0103@gmail.com>
2026-06-25 09:17:04 -0700

    Create 78-Subsets

---
+++ 78-Subsets/78-Subsets.py
@@ -0,0 +1,22 @@
+# Last updated: 6/25/2026, 9:17:04 AM
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res=[]
+        subset=[]
+
+        def backtrack(i):
+            if i>=len(nums):
+                res.append(subset[:])
+                return 
+            
+            subset.append(nums[i])
+            backtrack(i+1)
+            subset.pop()
+            backtrack(i+1)
+
+        backtrack(0)
+        return res
